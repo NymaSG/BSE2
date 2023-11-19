40f64deccbc8b1f60c5c4a397b76e8a6592a16a1
NymaSG <soltik001002003@mail.ru>
2023-11-19 19:25:20 +0300

    second changes

--- calculator.py
+++ calculator.py
@@ -3,9 +3,9 @@ def calculator():
         a = float(input("Введите первое число: "))
         b = float(input("Введите второе число: "))
         
-        result = a + b
+        c = a + b
     
-        print("Результат сложения:", result)
+        print("Результат сложения:", c)
         
     except ValueError:
         print("Ошибка ввода. Пожалуйста, введите числа.")
